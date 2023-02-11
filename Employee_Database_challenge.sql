ce40be9b054934f4f3f38d724f86cdc6305df0e0
alosmad <78031156+alosmad@users.noreply.github.com>
2023-02-10 22:31:12 -0600

    Add files via upload
    Deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -30,3 +30,23 @@ INTO retiring_titles
 FROM title_noduplicates as tn
 GROUP BY tn.title
 ORDER BY count DESC;
+
+-- Mentoship elegibility program
+
+SELECT DISTINCT ON (e.emp_no)
+e.emp_no,
+e.first_name, 
+e.last_name, 
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, t.from_date DESC;
